985d4fcffa777f86e1a215643e0510cff1d98852
ahsteele <ahsteele+git@gmail.com>
2011-01-21 16:41:05 -0700

    not working but complete program

--- src/edu/cmu/speech2text/Speech2Text.java
+++ src/edu/cmu/speech2text/Speech2Text.java
@@ -9,16 +9,22 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class Speech2Text extends Activity implements OnClickListener {
+public class Speech2Text extends Activity implements OnClickListener, OnInitListener  {
 	
 	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+	private static final int TEXT_TO_SPEECH_CODE = 007;
+	
 	private ListView mList;
+	private TextToSpeech tts;
 	
     /** Called when the activity is first created. */
     @Override
@@ -45,6 +51,11 @@ public class Speech2Text extends Activity implements OnClickListener {
         
         Button speakButton = (Button) findViewById(R.id.button_speak);
         speakButton.setOnClickListener(this);
+        
+        Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, TEXT_TO_SPEECH_CODE);
+
     }
 
 	@Override
@@ -53,7 +64,12 @@ public class Speech2Text extends Activity implements OnClickListener {
 			startVoiceRecognitionActivity();
 		}
 		else if (v.getId() == R.id.button_speak) {
+			String text = mList.getTextFilter().toString();
 			
+			if (text != null && text.length() > 0) {
+				Toast.makeText(Speech2Text.this, "Saying: " + text, Toast.LENGTH_LONG).show();
+				tts.speak(text, TextToSpeech.QUEUE_ADD, null);
+			}
 		}
 	}
 
@@ -72,8 +88,30 @@ public class Speech2Text extends Activity implements OnClickListener {
 				data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 			mList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
 					matches));
+		} else if (requestCode == TEXT_TO_SPEECH_CODE) {
+			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+				tts = new TextToSpeech(this, this);
+			}
+			else {
+				Intent installIntent = new Intent();
+				installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+				startActivity(installIntent);
+			}
 		}
 		
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onInit(int status) {
+		if (status == TextToSpeech.SUCCESS) {
+			Toast.makeText(Speech2Text.this,
+					"Text-To-Speech engine is initialized", Toast.LENGTH_LONG).show();
+		}
+		else if (status == TextToSpeech.ERROR) {
+			Toast.makeText(Speech2Text.this,
+					"Error occured while initializing Text-To-Speech engine",
+					Toast.LENGTH_LONG).show();
+		}
 	}
 }
